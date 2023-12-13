5dabb50814092136c5ff707c74877bb800b50649
Jahongir <sjahongir22@gmail.com>
2023-12-13 15:55:44 +0500

    ))

--- src/main/java/uz/pdp/learning_center_full/service/AttendanceService.java
+++ src/main/java/uz/pdp/learning_center_full/service/AttendanceService.java
@@ -91,6 +91,7 @@ public class AttendanceService {
 
     }
     public List<LessonAttendanceResponse> getAttendanceWithLessonByModule(UUID groupId,Integer module){
+
         System.out.println("module = " + module);
 //        int modulee = 1;
         List<LessonEntity> lessonEntityList = lessonRepository.findLessonEntitiesByGroupId(groupId);
