de4d158fea4f72d2075e3f632a781a35c9c75e0b
UzCoder <kimsanov0717@gmail.com>
2023-12-17 15:55:00 +0500

    1

--- src/main/java/uz/pdp/learning_center_full/controller/StudentController.java
+++ src/main/java/uz/pdp/learning_center_full/controller/StudentController.java
@@ -21,46 +21,7 @@ import java.util.UUID;
 public class StudentController {
     private final StudentService studentService;
     private final AttendanceService attendanceService;
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @PostMapping("/create")
-    public ResponseEntity<StudentResponse> create(@Valid @RequestBody StudentCR studentCR) {
-        return ResponseEntity.status(200).body(studentService.create(studentCR));
-    }
-
-    @PreAuthorize("hasRole('ADMIN')  or hasRole('STUDENT') or hasRole('SUPER_ADMIN')")
-    @PutMapping("/update")
-    public ResponseEntity<StudentResponse> update(
-            @RequestParam UUID studentId,
-            @RequestBody StudentUpdateDTO update) {
-        return ResponseEntity.status(200).body(studentService.updateById(studentId, update));
-    }
-
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @DeleteMapping("delete/{studentId}")
-    public ResponseEntity<String> deleteStudentById(@PathVariable UUID studentId) {
-        return ResponseEntity.status(200).body(studentService.deleteById(studentId));
-    }
-
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @GetMapping("/getAll")
-    public ResponseEntity<List<StudentResponse>> getAll(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size){
-        return ResponseEntity.status(200).body(studentService.getAll(page, size));
-    }
-
-    @PreAuthorize("hasRole('ADMIN')  or hasRole('MENTOR') or hasRole('SUPER_ADMIN')")
-    @GetMapping("/get-group-students/{group_id}")
-    public ResponseEntity<List<StudentResponse>> getGroupStudents(@PathVariable UUID group_id){
-        return ResponseEntity.status(200).body(studentService.getByGroupId(group_id));
-    }
-
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @PostMapping("/create_by_application")
-    public ResponseEntity<StudentResponse> createByApplication(@RequestParam UUID application_id, UUID group_id){
-        return studentService.createByApplication(application_id,group_id);
-    }
-    @PreAuthorize("hasRole('MENTOR') or hasRole('ADMIN') or hasRole('STUDENT') or hasRole('SUPER_ADMIN')")
+    @PreAuthorize("hasRole('STUDENT')")
     @GetMapping("/get_students_by_rating")
     public ResponseEntity<List<StudentResponse>> getStudentByRating(UUID groupId){
         return ResponseEntity.status(200).body(studentService.getStudentByRating(groupId));
