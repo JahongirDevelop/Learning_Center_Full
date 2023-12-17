d07c0a01b1553778c92c33844dd8833e1408d10a
UzCoder <kimsanov0717@gmail.com>
2023-12-17 14:54:37 +0500

    1

--- src/main/java/uz/pdp/learning_center_full/controller/ApplicationController.java
+++ src/main/java/uz/pdp/learning_center_full/controller/ApplicationController.java
@@ -18,28 +18,8 @@ import java.util.UUID;
 public class ApplicationController {
     private final ApplicationService applicationService;
     @PostMapping("/create")
-    public ResponseEntity<ApplicationResponse> create(@RequestBody ApplicationCr applicationCR){
+    public ResponseEntity<ApplicationResponse> create(@RequestBody ApplicationCr applicationCR) {
         return ResponseEntity.ok(applicationService.create(applicationCR));
-    }
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @GetMapping("/get_application{id}")
-    public ResponseEntity<ApplicationResponse> getApplication(@PathVariable UUID id){
-        return applicationService.findById(id);
-    }
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @GetMapping("/getAll")
-    public List<ApplicationResponse> getAll(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size){
-        return applicationService.getAll(page, size);
-    }
-    @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
-    @GetMapping("/getAllByStatus")
-    public List<ApplicationResponse> getAllBYStatus(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam ApplicationStatus status){
-        return applicationService.getAllByStatus(page, size,status);
     }
 }
 
